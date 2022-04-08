e5993a0b0c55f6a36d8e63fc28324aec547f6c97
TJLTM <tjltm@gmail.com>
2022-04-07 19:21:01 -0700

    initial. Need to get a few other parts before i put docs, schematics or anything else.

---
+++ FW/Garden_Soil_Moisture_Meter/Garden_Soil_Moisture_Meter.ino
@@ -0,0 +1,121 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// Update these with values suitable for your network.
+
+const char* ssid = "...";
+const char* password = "...";
+const char* mqtt_server = "...";
+
+String Name = "SoilMoisture";
+String ID;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is active low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  String MAC = WiFi.macAddress();
+  for (int x = 9; x < 17; x++) {
+    if (MAC.charAt(x) != ':') {
+      ID.concat(MAC.charAt(x));
+    }
+  }
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  digitalWrite(BUILTIN_LED,HIGH);
+  delay(500);
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  Serial.println(Name + "/" + ID);
+  Serial.println("Posting to MQTT");
+  String MoistTopic = Name + "/" + ID + "/Moisture";
+  client.publish(MoistTopic.c_str(),"Value");
+  String VoltTopic = Name + "/" + ID + "/Voltage";
+  client.publish(VoltTopic.c_str(),"Value");
+  Serial.println("going to sleep");
+  digitalWrite(BUILTIN_LED,LOW);
+  delay(500);
+  ESP.deepSleep(30e6);
+}
+
+void loop() {
+}
+
+  
+
+
+  
